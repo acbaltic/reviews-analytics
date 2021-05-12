3f32bd63f099051074f9be827ff4ab3405f5c12f
Andy Chin <acbaltic0630@gmail.com>
2021-05-12 23:06:39 +0800

    range 範圍-->清單產生器-->延伸

--- read.py
+++ read.py
@@ -56,4 +56,34 @@ print('一共有', len(good), '筆留言包含 good')
 print(good[100])
 '''
 
+# list comprehension-->清單快寫法-->進階用法
+'''
+data = []
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+good = ['bad' in d for d in data]
+print(good[101])
+'''
 
+# range 範圍-->清單產生器
+'''
+90%以上，我們寫這種for i in range()的for loop只是為了讓它的內容重複執行某個"固定次數"
+# 10是結尾值，不包含在內
+import random
+for i in range(100):
+    r = random.randint(1, 6)
+    print('這是第', i + 1, '次產生隨機數： ', r)
+'''
+
+# range 範圍-->清單產生器-->延伸
+'''
+data = range(2, 10, 3)
+print(data)
+for i in data:
+    print(i)
+
+data1 = list(range(2, 10, 3))
+data2 = list(range(10, 3, -2))
+print(data1, data2)
+'''
